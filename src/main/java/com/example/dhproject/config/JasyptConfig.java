0fc4c4e3b317d0549b2f92b60982d9cef2b13f66
dhy25n <sai4875@naver.com>
2024-08-07 17:13:21 +0900

    Jasypt - yml db 암호화

---
+++ src/main/java/com/example/dhproject/config/JasyptConfig.java
@@ -0,0 +1,39 @@
+package com.example.dhproject.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    public String jasyptPassword = "";
+
+    /**
+     * 암호화 설정
+     * @return
+     */
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        // 암호화에 사용할 키
+        config.setPassword(jasyptPassword);
+        // 사용할 알고리즘
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
