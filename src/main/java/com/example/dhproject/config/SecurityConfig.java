6065a38dd5c2a6af1d262eb2fe98dbd8f859e0cf
dhy25n <sai4875@naver.com>
2024-09-10 19:28:15 +0900

    수정 - security csrf

--- src/main/java/com/example/dhproject/config/SecurityConfig.java
+++ src/main/java/com/example/dhproject/config/SecurityConfig.java
@@ -21,10 +21,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
                                 .antMatchers("/css/**", "/js/user/**", "/images/**", "/plugins/**").permitAll()
-                                .antMatchers("/api/pubic/members/**").permitAll()
+                                .antMatchers(
+                                        "/api/pubic/members/**",
+                                        "/register",
+                                        "/api/public/members/send-verification-code",
+                                        "/api/public/members/verify-code",
+                                        "/api/public/members/check-email").permitAll()
                                 .antMatchers("/api/admin/members/**").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
@@ -37,9 +43,6 @@ public class SecurityConfig {
                                 .permitAll()
                 )
                 .logout(LogoutConfigurer::permitAll
-                )
-                .csrf(csrf -> csrf
-                        .ignoringAntMatchers("/api/members/register", "/api/members/send-verification-code", "/api/members/verify-code")
                 );
 
         return http.build();
